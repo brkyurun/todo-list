db4996fb88b0ac469a966a5ea8a91a5293209d42
berkay <berkayurun@outlook.com>
2022-04-28 15:14:30 +0300

    setup initial structure

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/app.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
+  },
+};
