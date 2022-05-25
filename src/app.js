6926a4cddefab4657e0985596878e76e980a36f6
berkay <berkayurun@outlook.com>
2022-05-25 16:40:58 +0300

    add sidebar component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import loadHeader from "./ui_components/Header";
+import loadProjects from "./ui_components/Projects";
 import { loadModalElements, attachListeners } from "./ui_components/Modal";
 
 loadHeader();
+loadProjects();
 loadModalElements();
 
 window.addEventListener("DOMContentLoaded", attachListeners);
--- src/ui_components/Projects.js
+++ src/ui_components/Projects.js
@@ -2,4 +2,54 @@ function createProject(name) {
   const project = document.createElement("div");
   const projectName = document.createElement("li");
   const removeProjectButton = document.createElement("button");
+
+  projectName.textContent = name;
+  projectName.classList.add("projectName");
+
+  removeProjectButton.type = "button";
+  removeProjectButton.textContent = "-";
+  removeProjectButton.classList.add("removeProject");
+
+  project.classList.add("project");
+
+  project.appendChild(projectName);
+  project.appendChild(removeProjectButton);
+
+  return project;
+}
+
+function createProjectsList() {
+  const projectsContainer = document.createElement("div");
+  const projectsList = document.createElement("ul");
+
+  projectsContainer.id = "projectsContainer";
+  projectsContainer.classList.add("projectsContainer");
+
+  projectsList.id = "projectsList";
+  projectsList.appendChild(createProject("Adelaide Ltd. & Co."));
+  projectsList.appendChild(createProject("Lemon Squeeze"));
+  projectsList.appendChild(createProject("Barbecue Party"));
+  projectsList.appendChild(createProject("Work from Home"));
+
+  projectsContainer.appendChild(projectsList);
+
+  return projectsContainer;
+}
+
+function createSidebar() {
+  const aside = document.createElement("aside");
+  const title = document.createElement("h2");
+
+  title.textContent = "Projects";
+
+  aside.appendChild(title);
+  aside.appendChild(createProjectsList());
+
+  return aside;
+}
+
+export default function loadProjects() {
+  const body = document.querySelector("body");
+
+  body.appendChild(createSidebar());
 }
