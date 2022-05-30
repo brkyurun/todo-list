0cecd81978da0460089b1cee6a743c4c1cfded1a
Berkay <berkayurun@outlook.com>
2022-05-30 13:50:53 +0300

    translate js to html

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
-import "./styles.css";
-import loadHeader from "./ui_components/Header";
-import createSidebar from "./ui_components/Projects";
-import createTodoContainer from "./ui_components/Tasks";
-import { loadModalElements, attachListeners } from "./ui_components/Modal";
-import loadFooter from "./ui_components/Footer";
+const openModal = document.querySelector("#open-modal");
+const modal = document.querySelector("#modal-container");
+const closeModal = document.querySelector("#close-modal");
+const textArea = document.querySelector("#task-description");
 
-const body = document.querySelector("body");
-const projectsAndTodosContainer = document.createElement("main");
-projectsAndTodosContainer.appendChild(createSidebar());
-projectsAndTodosContainer.appendChild(createTodoContainer());
+openModal.addEventListener("click", () => modal.classList.add("show"));
+closeModal.addEventListener("click", () => modal.classList.remove("show"));
 
-loadHeader();
-body.appendChild(projectsAndTodosContainer);
-loadModalElements();
-loadFooter();
+window.addEventListener("keydown", (e) => {
+  if (modal.classList.contains("show") && e.key === "Escape") {
+    modal.classList.remove("show");
+  }
+});
 
-window.addEventListener("DOMContentLoaded", attachListeners);
+textArea.addEventListener("input", resizeArea);
+
+function resizeArea() {
+  this.style.height = this.scrollHeight + "px";
+}
+
+// todos:
+// - create new ui components to create projects and tasks
+// - create controller modules for projects and tasks to handle logic and data
+// - projects should have empty arrays when created, and render from that array of objects to create UI elements
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,14 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: "./src/app.js",
-  plugins: [new MiniCssExtractPlugin()],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
     static: "./dist",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
   },
 };
